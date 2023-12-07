d98e5e5d169086505fca8d13b448047bddc7ea14
iammoonman <420saas@gmail.com>
2023-12-07 08:44:13 -0500

    version number

--- Magic/Importer.english.lua
+++ Magic/Importer.english.lua
@@ -1,5 +1,5 @@
 --By Amuzet
-mod_name, version = 'Card Importer', 1.966
+mod_name, version = 'Card Importer', 1.970
 self.setName('[854FD9]' .. mod_name .. ' [49D54F]' .. version)
 author, WorkshopID, GITURL = '76561198045776458', 'https://steamcommunity.com/sharedfiles/filedetails/?id=1838051922', 'https://raw.githubusercontent.com/Amuzet/Tabletop-Simulator-Scripts/master/Magic/Importer.lua'
 coauthor = '76561197968157267' --PIE
